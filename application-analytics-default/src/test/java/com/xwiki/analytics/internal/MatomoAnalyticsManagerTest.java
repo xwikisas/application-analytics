301b284a5a54d5063bed61c5d92172478e8eed22
Farcasut <farcasialexandrububu@gmail.com>
2023-08-11 13:02:43 +0300

    Move Most Viewed Pages Macro to livetable #10 * I added a test for the MatomoANalyticsManager

--- application-analytics-default/src/test/java/com/xwiki/analytics/internal/MatomoAnalyticsManagerTest.java
+++ application-analytics-default/src/test/java/com/xwiki/analytics/internal/MatomoAnalyticsManagerTest.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package com.xwiki.analytics.internal;
 
 import java.io.IOException;
@@ -18,6 +37,7 @@ import com.xwiki.analytics.JsonNormaliser;
 import com.xwiki.analytics.configuration.AnalyticsConfiguration;
 
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ComponentTest
 public class MatomoAnalyticsManagerTest
@@ -38,8 +58,8 @@ public class MatomoAnalyticsManagerTest
         CreateResourceTypeException, CreateResourceReferenceException
     {
         when(this.configuration.getAuthenticationToken()).thenReturn("token");
-        when(this.configuration.getRequestAddress()).thenReturn("http://130.61.233.19//matomo");
+        when(this.configuration.getRequestAddress()).thenReturn("http://130.61.233.19/matomo");
         when(this.configuration.getIdSite()).thenReturn("3");
-        this.matomoAnalyticsManager.requestData(new HashMap<>(), "MostViewedPages");
+        assertEquals(null,  this.matomoAnalyticsManager.requestData(new HashMap<>(), "MostViewedPages"));
     }
 }
