3eb23a57b583b5fa6eb0d85ce4892c00bbeb753f
Farcasut <farcasialexandrububu@gmail.com>
2023-09-15 14:29:23 +0300

    test

---
+++ application-analytics-test/application-analytics-test-pageobjects/src/main/java/com/xwiki/analytics/test/po/HomePageViewPage.java
@@ -0,0 +1,19 @@
+package com.xwiki.analytics.test.po;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.xwiki.test.ui.po.ViewPage;
+
+public class HomePageViewPage extends ViewPage
+{
+    public HomePageViewPage()
+    {
+
+    }
+    public static ApplicationIndexHomePage gotoPage()
+    {
+
+    }
+}
