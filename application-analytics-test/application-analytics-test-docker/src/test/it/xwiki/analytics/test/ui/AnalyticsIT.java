65722951f3da03477c2d733c438649af6cd1bbbc
Farcasut <farcasialexandrububu@gmail.com>
2023-09-17 23:46:57 +0300

    functional tests

--- application-analytics-test/application-analytics-test-docker/src/test/it/xwiki/analytics/test/ui/AnalyticsIT.java
+++ application-analytics-test/application-analytics-test-docker/src/test/it/xwiki/analytics/test/ui/AnalyticsIT.java
@@ -19,9 +19,19 @@
  */
 package xwiki.analytics.test.ui;
 
+import javax.inject.Inject;
+
+import org.hibernate.validator.constraints.ru.INN;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
 import org.xwiki.application.test.po.ApplicationIndexHomePage;
+import org.xwiki.test.docker.internal.junit5.DockerTestUtils;
+import org.xwiki.test.docker.junit5.TestConfiguration;
 import org.xwiki.test.docker.junit5.UITest;
 import org.xwiki.test.docker.junit5.servletengine.ServletEngine;
 import org.xwiki.test.ui.TestUtils;
@@ -30,19 +40,53 @@ import org.xwiki.administration.test.po.AdministrationPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.xwiki.test.ui.po.ViewPage;
+
+import com.xwiki.analytics.test.po.HomePageViewPage;
+
+import java.util.Collections;
+import java.util.List;
 
 @UITest
 public class AnalyticsIT
 {
+    @BeforeAll
+    void congig(TestConfiguration testConfiguration)
+    {
+
+        GenericContainer<?> mariadb =
+            new GenericContainer<>(DockerImageName.parse("mariadb")).withEnv("MYSQL_ROOT_PASSWORD",
+                    "exampleRootPassword").withEnv("MYSQL_DATABASE", "matomo").withEnv("MYSQL_USER", "matomo")
+                .withEnv("MYSQL_PASSWORD", "examplePassword").withExposedPorts(3306)
+                .waitingFor(Wait.forListeningPort());
+
+        // Define the Matomo container
+        GenericContainer<?> matomo =
+            new GenericContainer<>(DockerImageName.parse("matomo")).withEnv("MATOMO_DATABASE_HOST", mariadb.getHost())
+                .withEnv("MATOMO_DATABASE_USERNAME", "matomo").withEnv("MATOMO_DATABASE_PASSWORD", "examplePassword")
+                .withEnv("MATOMO_DATABASE_DBNAME", "matomo").withExposedPorts(80).dependsOn(mariadb)
+                .waitingFor(Wait.forHttp("/"));
+
+/*        GenericContainer genericContainer = new GenericContainer("matomo");
+        genericContainer.setPortBindings(Collections.singletonList("9999:80"));*/
+                   mariadb.setPortBindings(Collections.singletonList("9999:3306")); // Map host port 9999 to container port 3306
+            matomo.setPortBindings(Collections.singletonList("9998:80"));    // Map host port 9998 to container port 80
+        try {
+            DockerTestUtils.startContainer(mariadb, testConfiguration);
+            DockerTestUtils.startContainer(matomo, testConfiguration);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
     void appEntryRedirectsToHomePage(XWikiWebDriver driver, TestUtils setup)
     {
 
-        ApplicationIndexHomePage applicationIndexHomePage = ApplicationIndexHomePage.gotoPage();
-        System.out.println(applicationIndexHomePage.getPageURL());
-        assertFalse(true);
-
-
+        HomePageViewPage.gotoPage();
+        while (true) {
+            System.out.print("1");
+        }
     }
 }
