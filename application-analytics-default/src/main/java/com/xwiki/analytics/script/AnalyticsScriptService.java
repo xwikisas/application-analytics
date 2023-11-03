23c4a439746c7fbe924b9ecaab660b77e73a45b2
farcasut <farcasialexandrububu@gmail.com>
2023-11-03 11:08:14 +0200

    Improve rights administration #7  * - Removed unstable annotation - Modified the version of @since to be 1.0

--- application-analytics-default/src/main/java/com/xwiki/analytics/script/AnalyticsScriptService.java
+++ application-analytics-default/src/main/java/com/xwiki/analytics/script/AnalyticsScriptService.java
@@ -60,9 +60,7 @@ public class AnalyticsScriptService implements ScriptService
      * @param filters holds the criteria for filtering a dataset.
      * @param parameters a map of the parameters needed for this request
      * @return response from Matomo API, in a normalized JSON format
-     * @since 1.0
      */
-    @Unstable
     public JsonNode getMatomoRequestResult(Map<String, String> parameters, Map<String, String> filters,
         String jsonNormaliserHint)
     {
@@ -76,9 +74,7 @@ public class AnalyticsScriptService implements ScriptService
     /**
      *
      * @return the analytics configuration.
-     * @since 1.0
      */
-    @Unstable
     public AnalyticsConfiguration getConfiguration()
     {
         return configuration;
