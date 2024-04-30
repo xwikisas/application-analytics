df03ef6b3b9e607e96b62479c5c94a680f7ccc21
farcasut <farcasialexandrububu@gmail.com>
2024-04-30 15:05:20 +0300

    Removed the real requests from the unit tests. #96
     Added a test back

--- application-analytics-default/src/test/java/com/xwiki/analytics/internal/MatomoAnalyticsManagerTest.java
+++ application-analytics-default/src/test/java/com/xwiki/analytics/internal/MatomoAnalyticsManagerTest.java
@@ -27,23 +27,18 @@ import java.util.List;
 import javax.inject.Named;
 import javax.inject.Provider;
 
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.HttpEntity;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.xwiki.component.util.ReflectionUtils;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
 import org.xwiki.test.junit5.mockito.MockComponent;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xwiki.analytics.JsonNormaliser;
 import com.xwiki.analytics.configuration.AnalyticsConfiguration;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -52,6 +47,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+
 /**
  * Unit test for {@link MatomoAnalyticsManager}
  *
@@ -75,6 +71,33 @@ public class MatomoAnalyticsManagerTest
 
     @MockComponent
     private Provider<List<JsonNormaliser>> jsonNormalizerProvider;
+
+    @MockComponent
+    private HttpClientBuilderFactory builderFactory;
+
+    /**
+     * Will test the Manager with a valid hint.
+     */
+    @Test
+    void requestDataWithCorrectHintForNormaliser() throws IOException
+    {
+        List<JsonNormaliser> normalisers = new ArrayList<>();
+        HttpClient mockClient = mock(HttpClient.class);
+        HttpResponse mockResponse = mock(HttpResponse.class);
+        HttpEntity mockEntity = mock(HttpEntity.class);
+
+        normalisers.add(this.jsonNormaliser);
+        when(this.configuration.getAuthenticationToken()).thenReturn("token");
+        when(this.configuration.getRequestAddress()).thenReturn("https://matomo-url/");
+        when(this.configuration.getIdSite()).thenReturn("3");
+        when(this.jsonNormalizerProvider.get()).thenReturn(normalisers);
+        when(this.jsonNormaliser.getIdentifier()).thenReturn(MostViewedJsonNormaliser.HINT);
+        when(this.builderFactory.create()).thenReturn(mockClient);
+        when(mockClient.execute(any())).thenReturn(mockResponse);
+        when(mockResponse.getEntity()).thenReturn(mockEntity);
+        this.matomoAnalyticsManager.requestData(new HashMap<>(), new HashMap<>(), MostViewedJsonNormaliser.HINT);
+        verify(this.jsonNormaliser).normaliseData(any(), eq(new HashMap<>()));
+    }
 
     /**
      * Will test that an error happens if the user sets the parameters to be equal with null.
