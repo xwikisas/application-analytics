248dd44a0535d0a4c48b6f7293aac8237ebd1297
farcasut <farcasialexandrububu@gmail.com>
2024-04-30 09:13:37 +0300

    Removed test.

--- application-analytics-default/src/test/java/com/xwiki/analytics/internal/MatomoAnalyticsManagerTest.java
+++ application-analytics-default/src/test/java/com/xwiki/analytics/internal/MatomoAnalyticsManagerTest.java
@@ -27,23 +27,31 @@ import java.util.List;
 import javax.inject.Named;
 import javax.inject.Provider;
 
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.xwiki.component.util.ReflectionUtils;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
 import org.xwiki.test.junit5.mockito.MockComponent;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xwiki.analytics.JsonNormaliser;
 import com.xwiki.analytics.configuration.AnalyticsConfiguration;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 /**
  * Unit test for {@link MatomoAnalyticsManager}
  *
@@ -67,23 +75,6 @@ public class MatomoAnalyticsManagerTest
 
     @MockComponent
     private Provider<List<JsonNormaliser>> jsonNormalizerProvider;
-
-    /**
-     * Will test the Manager with a valid hint.
-     */
-    @Test
-    void requestDataWithCorrectHintForNormaliser() throws IOException
-    {
-        List<JsonNormaliser> normalisers = new ArrayList<>();
-        normalisers.add(this.jsonNormaliser);
-        when(this.configuration.getAuthenticationToken()).thenReturn("token");
-        when(this.configuration.getRequestAddress()).thenReturn("http://130.61.233.19/matomo");
-        when(this.configuration.getIdSite()).thenReturn("3");
-        when(this.jsonNormalizerProvider.get()).thenReturn(normalisers);
-        when(this.jsonNormaliser.getIdentifier()).thenReturn(MostViewedJsonNormaliser.HINT);
-        this.matomoAnalyticsManager.requestData(new HashMap<>(), new HashMap<>(), MostViewedJsonNormaliser.HINT);
-        verify(this.jsonNormaliser).normaliseData(any(String.class), eq(new HashMap<>()));
-    }
 
     /**
      * Will test that an error happens if the user sets the parameters to be equal with null.
